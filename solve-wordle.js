1aec9c46c35474fb921b3ee723d663f2fe643c02
DJSdev <26153156+DJSdev@users.noreply.github.com>
2022-02-12 17:00:21 -0500

    updated extra var

--- solve-wordle.js
+++ solve-wordle.js
@@ -1,11 +1,12 @@
 const words = require("./words");
 
-var seedDate = new Date(2021, 5, 19, 0, 0, 0, 0);
+let seedDate = new Date(2021, 5, 19, 0, 0, 0, 0);
+let seedNum = 864e5;
 
 function getSeed(todaysDate) {
   var dateDiff =
     new Date(todaysDate).setHours(0, 0, 0, 0) - seedDate.setHours(0, 0, 0, 0);
-  return Math.round(dateDiff / 864e5);
+  return Math.round(dateDiff / seedNum);
 }
 
 function getSolution(todaysDate) {
